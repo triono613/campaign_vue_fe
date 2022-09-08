096162a3a4ce1849f890eb6fd05afbaefb81831a
komang  bp <komangbp@yahoo.com>
2022-09-08 17:11:24 +0700

    new source

---
+++ components/layouts/Error.vue
@@ -0,0 +1,34 @@
+<template>
+     <div class="container mx-auto h-screen flex justify-center items-center">
+        <div class="w-full lg:w-1/3 px-10 lg:px-0">
+          <div class="flex justify-center items-center mx-auto mt-6 mb-8">
+            <img src="/404-illustration.svg" alt="" class="w-full" />
+          </div>
+          <h2 class="font-medium mb-3 text-3xl text-center">
+            Welcome onboard!
+          </h2>
+          <p class="text-center font-light" v-if="error.statuscode === 404">
+            The page that you request doesnt  
+            <br />
+            exist at this moment
+          </p>
+          <p class="text-center font-light" v-else>an orror accured</p>
+          <div class="mb-4 mt-6">
+            <div class="mb-3">
+              <button
+                @click="$router.push({ path: '/' })"
+                class="block w-full bg-orange-button hover:bg-green-button text-white font-semibold px-6 py-4 text-lg rounded-full"
+              > Back to home
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+</template>
+
+<script>
+    export default {
+        props: ['error'],
+        layout : 'success'
+    }
+</script>
